e0a8a1d7987c54dab904d03d456a4f6c51f6beff
exfsys <67659981+exfsys@users.noreply.github.com>
2020-08-20 20:05:37 +0300

    Initial commit

---
+++ MPL Tool UI/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace MPL_Tool_UI
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ MPL Tool UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MPL_Tool_UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
